b9062e02cdaa5d9081d97f64f9d65a8684998598
matttoppi <47908195+matttoppi@users.noreply.github.com>
2024-09-15 17:12:47 -0400

    final test hopefully

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
-  //testing
   try {
     await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL']!,
