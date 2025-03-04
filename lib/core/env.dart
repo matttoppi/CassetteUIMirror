55dccfa2438b4c49b6761224fafd51182a8228e4
matttoppi <47908195+matttoppi@users.noreply.github.com>
2025-03-04 12:03:51 -0500

    removal of unneeded

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -12,11 +12,6 @@ class Env {
       ? dotenv.env['APP_DOMAIN_DEV'] ?? 'https://localhost:56752'
       : dotenv.env['APP_DOMAIN_PROD'] ?? 'https://cassetteinc.org';
 
-  // Production
-  // const supabaseUrl = "https://wefxtfnobcmlzysornxw.supabase.co";
-  // const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlZnh0Zm5vYmNtbHp5c29ybnh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ2MTAyOTQsImV4cCI6MjAzMDE4NjI5NH0.OW-VZZB4fRevnkSIwh7w8iApJ8W7QNk9Mf1cGVJ1z-s";
-  // const profileBucket = 'profile-pictures';
-  // const appDomain = 'https://cassetteinc.org';
 
   // Production values can be added as needed
   // static const prodSupabaseUrl = "...";
