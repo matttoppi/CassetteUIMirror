110824370f9fe7aa5b3ac223fe29b43047c4abef
matttoppi <47908195+matttoppi@users.noreply.github.com>
2025-03-05 20:41:11 -0500

    modified build script

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ flutter/bin/flutter build web --release \
   --dart-define=API_ENV="${API_ENV:-prod}" \
   --dart-define=SUPABASE_URL="$SUPABASE_URL" \
   --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" \
-  --dart-define=SPOTIFY_API_KEY="$SPOTIFY_API_KEY"
+  --dart-define=SPOTIFY_CLIENT_ID="$SPOTIFY_CLIENT_ID" \
+  --dart-define=SPOTIFY_CLIENT_SECRET="$SPOTIFY_CLIENT_SECRET"
 
 echo "Build completed. Listing build/web directory:"
 ls -la build/web
