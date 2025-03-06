5a8e1c2132af7b01a73e621145d532e2680231be
matttoppi <47908195+matttoppi@users.noreply.github.com>
2025-03-06 03:04:10 -0500

    new buildscript

--- build.sh
+++ build.sh
@@ -13,7 +13,10 @@ flutter/bin/flutter build web --release \
   --dart-define=SUPABASE_URL="$SUPABASE_URL" \
   --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" \
   --dart-define=SPOTIFY_CLIENT_ID="$SPOTIFY_CLIENT_ID" \
-  --dart-define=SPOTIFY_CLIENT_SECRET="$SPOTIFY_CLIENT_SECRET"
+  --dart-define=SPOTIFY_CLIENT_SECRET="$SPOTIFY_CLIENT_SECRET" \
+  --dart-define=APPLE_MUSIC_TEAM_ID="$APPLE_MUSIC_TEAM_ID" \
+  --dart-define=APPLE_MUSIC_KEY_ID="$APPLE_MUSIC_KEY_ID" \
+  --dart-define=APPLE_MUSIC_PRIVATE_KEY="$APPLE_MUSIC_PRIVATE_KEY"
 
 echo "Build completed. Listing build/web directory:"
 ls -la build/web
